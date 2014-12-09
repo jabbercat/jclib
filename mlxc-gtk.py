5f2c37b5aaf768aa0ebfc5430ede392f4707fefa
Jonas Wielicki <j.wielicki@sotecware.net>
2014-12-09 14:23:07 +0100

    Add Qt example and make both examples equal

--- mlxc-gtk.py
+++ mlxc-gtk.py
@@ -11,52 +11,18 @@ loop = asyncio.get_event_loop()
 builder = Gtk.Builder()
 builder.add_from_file("data/core.glade")
 
-def foo(window, event):
-    # window.hide()
-    # return True
-    return False
-
 main_window = builder.get_object("roster")
-main_window.connect("delete_event", foo)
 
 def cb(*args, **kwargs):
     app.add_window(main_window)
     main_window.show_all()
 
-
 app = Gtk.Application()
 app.connect("activate", cb, None)
 
 @asyncio.coroutine
-def foo():
-    status_icon = builder.get_object("statusicon1")
-    print(dir(status_icon))
-    status_icon.set_from_stock(Gtk.STOCK_NEW)
-    status_icon.set_visible(True)
-    try:
-        while True:
-            # print("foo")
-            # dlg = Gtk.MessageDialog(
-            #     None,
-            #     Gtk.DialogFlags.MODAL,
-            #     Gtk.MessageType.INFO,
-            #     Gtk.ButtonsType.YES_NO)
-            # dlg.set_markup("Hello World!")
-            # fut = gbulb.wait_signal(dlg, "response")
-            # dlg.show_all()
-            # yield from fut
-            # print(fut.result())
-            # dlg.close()
-            yield from asyncio.sleep(1)
-    except Exception as err:
-        print("err: ", err)
-        raise
-    finally:
-        print("exiting")
+def task():
+    while True:
+        yield from asyncio.sleep(1)
 
-task = asyncio.async(foo())
-try:
-    loop.run_forever(application=app)
-finally:
-    task.cancel()
-    loop.run_until_complete(task)
+loop.run_until_complete(task(), application=app)
