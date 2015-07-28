6be9d85ac4792834209d52e802a5544c261520f5
Jonas Wielicki <j.wielicki@sotecware.net>
2015-07-29 00:15:31 +0200

    Add functions to open XDG BaseDirectory files

--- mlxc/utils.py
+++ mlxc/utils.py
@@ -1,4 +1,8 @@
+import os.path
 import types
+import xdg.BaseDirectory
+
+import aioxmpp.errors
 
 from aioxmpp.utils import namespaces
 
@@ -6,3 +10,113 @@ from aioxmpp.utils import namespaces
 mlxc_namespaces = types.SimpleNamespace()
 mlxc_namespaces.roster = "https://xmlns.zombofant.net/mlxc/core/roster/1.0"
 mlxc_namespaces.account = "https://xmlns.zombofant.net/mlxc/core/account/1.0"
+
+
+def multiopen(paths, name, mode, *args, **kwargs):
+    """
+    Attempt to open a file called `name`, using multiple base paths given as
+    iterable `paths`.
+
+    `mode` is passed to :func:`open`, as well as the other `args` and `kwargs`.
+
+    Return the first file which gets opened successfully. If no file can be
+    opened, a :class:`aioxmpp.errors.MultiOSError` is raised with all the
+    exceptions which were raised by the individial :func:`open` calls
+    attached.
+    """
+    excs = []
+    for path in paths:
+        try:
+            return open(os.path.join(path, name), mode, *args, **kwargs)
+        except OSError as exc:
+            excs.append(exc)
+    raise aioxmpp.errors.MultiOSError("multiopen failed", excs)
+
+
+def xdgopen_generic(resource, name, mode, load_paths, save_path, **kwargs):
+    """
+    This generic open function is used for opening :mod:`xdg.BaseDirectory`
+    related files.
+
+    If the `mode` is a read-only mode, the paths obtained by calling
+    `load_paths` are passed in reverse order to :func:`multiopen` (along with
+    `name`, `mode` and the `kwargs`).
+
+    If the `mode` is not a read-only mode, the path obtained by calling
+    `save_path` is combined with `name` using :func:`os.path.join` and passed
+    to :func:`open` (along with `mode` and the `kwargs`).
+
+    The result of the respective function is returned.
+    """
+    if not mode.startswith("r") or "+" in mode:
+        return open(os.path.join(save_path(*resource), name),
+                    mode=mode,
+                    **kwargs)
+    paths = list(load_paths(*resource))
+    paths.reverse()
+    return multiopen(paths, name, mode=mode, **kwargs)
+
+
+def xdgconfigopen(*args, mode="rb", **kwargs):
+    """
+    Open a configuration file. The last of the positional arguments is used as
+    a file name, the others are taken as resource (see
+    :mod:`xdg.BaseDirectory`).
+
+    This function calls :func:`xdgopen_generic` and returns its result. The
+    :func:`xdg.BaseDirectory.load_config_paths` and
+    :func:`xdg.BaseDirectory.save_config_path` functions are used as values for
+    the `load_paths` and `save_path` arguments, respectively, to
+    :func:`xdgopen_generic`. The `mode` and the `kwargs` are passed along, as
+    well as the resource and the file name (as extracted from the positional
+    arguments).
+
+    To open the first matching config file ``fnord.xml`` for reading with a
+    resource of ``zombofant.net/mlxc``, one would call::
+
+        import mlxc.utils
+        f = mlxc.utils.xdgconfigopen("zombofant.net", "mlxc", "fnord.xml")
+
+    For writing, we would pass a different `mode`.
+    """
+
+    *resource, name = args
+    return xdgopen_generic(
+        resource,
+        name,
+        mode,
+        xdg.BaseDirectory.load_config_paths,
+        xdg.BaseDirectory.save_config_path,
+        **kwargs)
+
+
+def xdgdataopen(*args, mode="rb", **kwargs):
+    """
+    Open a data file. The last of the positional arguments is used as a file
+    name, the others are taken as resource (see :mod:`xdg.BaseDirectory`).
+
+    This function calls :func:`xdgopen_generic` and returns its result. The
+    :func:`xdg.BaseDirectory.load_data_paths` and
+    :func:`xdg.BaseDirectory.save_data_path` functions are used as values for
+    the `load_paths` and `save_path` arguments, respectively, to
+    :func:`xdgopen_generic`. The `mode` and the `kwargs` are passed along, as
+    well as the resource and the file name (as extracted from the positional
+    arguments).
+
+    To open the first matching data file ``foo.xml`` for reading with a
+    resource of ``zombofant.net/mlxc``, one would call::
+
+        import mlxc.utils
+        f = mlxc.utils.xdgdataopen("zombofant.net", "mlxc", "foo.xml")
+
+    For writing, we would pass a different `mode`.
+    """
+
+    *resource, name = args
+    return xdgopen_generic(
+        resource,
+        name,
+        mode,
+        xdg.BaseDirectory.load_data_paths,
+        xdg.BaseDirectory.save_data_path,
+        **kwargs)
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,11 @@
+import contextlib
+import os.path
 import unittest
+import unittest.mock
 
+import xdg.BaseDirectory
+
+import aioxmpp.errors
 import aioxmpp.utils as aioxmpp_utils
 
 import mlxc.utils as utils
@@ -24,4 +30,283 @@ class Testmlxc_namespaces(unittest.TestCase):
         self.assertEqual(
             "https://xmlns.zombofant.net/mlxc/core/roster/1.0",
             utils.mlxc_namespaces.roster
+        )
+
+
+class Testmultiopen(unittest.TestCase):
+    def setUp(self):
+        self.paths = [
+            "/foo/bar",
+            "/baz",
+            "/fnord",
+        ]
+
+    def test_first_success(self):
+        mode = object()
+        encoding = object()
+        obj = object()
+
+        with contextlib.ExitStack() as stack:
+            open_ = stack.enter_context(unittest.mock.patch(
+                "builtins.open"
+            ))
+            open_.return_value = obj
+
+            result = utils.multiopen(
+                self.paths,
+                "foo.xml",
+                mode,
+                encoding=encoding)
+
+        self.assertIs(
+            result,
+            obj)
+
+        self.assertSequenceEqual(
+            open_.mock_calls,
+            [
+                unittest.mock.call(
+                    os.path.join(self.paths[0], "foo.xml"),
+                    mode,
+                    encoding=encoding)
+            ]
+        )
+
+    def test_tries_and_returns_first_success(self):
+        mode = object()
+        encoding = object()
+        obj = object()
+
+        excs = [FileNotFoundError() for i in range(len(self.paths)-1)]
+        excs_to_use = list(excs)
+        call_rec = unittest.mock.Mock()
+
+        def open_mock(*args, **kwargs):
+            call_rec(*args, **kwargs)
+            if excs_to_use:
+                raise excs_to_use.pop(0)
+            return obj
+
+        with contextlib.ExitStack() as stack:
+            open_ = stack.enter_context(unittest.mock.patch(
+                "builtins.open",
+                open_mock
+            ))
+
+            result = utils.multiopen(
+                self.paths,
+                "foo.xml",
+                mode,
+                encoding=encoding)
+
+        self.assertSequenceEqual(
+            call_rec.mock_calls,
+            [
+                unittest.mock.call(
+                    os.path.join(path, "foo.xml"),
+                    mode,
+                    encoding=encoding)
+                for path in self.paths
+            ]
+        )
+
+
+class Testxdgopen_generic(unittest.TestCase):
+    def test_open_r(self):
+        paths = ["/foo/bar", "/fnord", "/baz"]
+
+        base = unittest.mock.Mock()
+        base.load_paths.return_value = iter(paths)
+        resource = ["foo", "bar"]
+        encoding = object()
+
+        with unittest.mock.patch("mlxc.utils.multiopen") as multiopen:
+            utils.xdgopen_generic(
+                resource,
+                "foo.xml",
+                "rb",
+                load_paths=base.load_paths,
+                save_path=base.save_path,
+                encoding=encoding
+            )
+
+        self.assertSequenceEqual(
+            base.mock_calls,
+            [
+                unittest.mock.call.load_paths(*resource),
+            ]
+        )
+
+        self.assertSequenceEqual(
+            multiopen.mock_calls,
+            [
+                unittest.mock.call(
+                    list(reversed(paths)),
+                    "foo.xml",
+                    mode="rb",
+                    encoding=encoding
+                )
+            ]
+        )
+
+    def test_open_w(self):
+        path = "/foo/bar"
+
+        base = unittest.mock.Mock()
+        base.save_path.return_value = path
+        resource = ["foo", "bar"]
+        encoding = object()
+
+        with unittest.mock.patch("builtins.open") as open_:
+            utils.xdgopen_generic(
+                resource,
+                "foo.xml",
+                "wb",
+                load_paths=base.load_paths,
+                save_path=base.save_path,
+                encoding=encoding
+            )
+
+        self.assertSequenceEqual(
+            base.mock_calls,
+            [
+                unittest.mock.call.save_path(*resource),
+            ]
+        )
+
+        self.assertSequenceEqual(
+            open_.mock_calls,
+            [
+                unittest.mock.call(
+                    os.path.join(path, "foo.xml"),
+                    mode="wb",
+                    encoding=encoding
+                )
+            ]
+        )
+
+    def test_open_a(self):
+        path = "/foo/bar"
+
+        base = unittest.mock.Mock()
+        base.save_path.return_value = path
+        resource = ["foo", "bar"]
+        encoding = object()
+
+        with unittest.mock.patch("builtins.open") as open_:
+            utils.xdgopen_generic(
+                resource,
+                "foo.xml",
+                "ab",
+                load_paths=base.load_paths,
+                save_path=base.save_path,
+                encoding=encoding
+            )
+
+        self.assertSequenceEqual(
+            base.mock_calls,
+            [
+                unittest.mock.call.save_path(*resource),
+            ]
+        )
+
+        self.assertSequenceEqual(
+            open_.mock_calls,
+            [
+                unittest.mock.call(
+                    os.path.join(path, "foo.xml"),
+                    mode="ab",
+                    encoding=encoding
+                )
+            ]
+        )
+
+    def test_open_rplus(self):
+        path = "/foo/bar"
+
+        base = unittest.mock.Mock()
+        base.save_path.return_value = path
+        resource = ["foo", "bar"]
+        encoding = object()
+
+        with unittest.mock.patch("builtins.open") as open_:
+            utils.xdgopen_generic(
+                resource,
+                "foo.xml",
+                "r+b",
+                load_paths=base.load_paths,
+                save_path=base.save_path,
+                encoding=encoding
+            )
+
+        self.assertSequenceEqual(
+            base.mock_calls,
+            [
+                unittest.mock.call.save_path(*resource),
+            ]
+        )
+
+        self.assertSequenceEqual(
+            open_.mock_calls,
+            [
+                unittest.mock.call(
+                    os.path.join(path, "foo.xml"),
+                    mode="r+b",
+                    encoding=encoding
+                )
+            ]
+        )
+
+
+class Testxdgconfigopen(unittest.TestCase):
+    def test_delegate_to_xdgopen_generic(self):
+        mode = "fnord"
+        encoding = object()
+
+        with unittest.mock.patch(
+                "mlxc.utils.xdgopen_generic"
+        ) as xdgopen_generic:
+            utils.xdgconfigopen("foo", "bar", "baz.xml",
+                                mode=mode,
+                                encoding=encoding)
+
+        self.assertSequenceEqual(
+            xdgopen_generic.mock_calls,
+            [
+                unittest.mock.call(
+                    ["foo", "bar"],
+                    "baz.xml",
+                    mode,
+                    xdg.BaseDirectory.load_config_paths,
+                    xdg.BaseDirectory.save_config_path,
+                    encoding=encoding
+                )
+            ]
+        )
+
+
+class Testxdgdataopen(unittest.TestCase):
+    def test_delegate_to_xdgopen_generic(self):
+        mode = "fnord"
+        encoding = object()
+
+        with unittest.mock.patch(
+                "mlxc.utils.xdgopen_generic"
+        ) as xdgopen_generic:
+            utils.xdgdataopen("foo", "bar", "baz.xml",
+                              mode=mode,
+                              encoding=encoding)
+
+        self.assertSequenceEqual(
+            xdgopen_generic.mock_calls,
+            [
+                unittest.mock.call(
+                    ["foo", "bar"],
+                    "baz.xml",
+                    mode,
+                    xdg.BaseDirectory.load_data_paths,
+                    xdg.BaseDirectory.save_data_path,
+                    encoding=encoding
+                )
+            ]
         )
